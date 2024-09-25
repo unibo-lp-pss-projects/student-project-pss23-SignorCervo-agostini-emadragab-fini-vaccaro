0c369c28e5cc35a836c2d94fb796ddc8e48cac56
Roberto Casadei <roby.casadei@unibo.it>
2024-09-25 14:55:47 +0200

    machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,19 @@
  */
 
 plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.develocity") version "3.18.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        buildScanPublished {
+            file("scan-journal.log").writeText("$buildScanId - $buildScanUri\n")
+        }
+    }
 }
 
 rootProject.name = "SignorCervo"
